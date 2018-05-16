e60778df20f55b1384b2ebe99c3d3e4a260973de
Robert Rajakone <robert.rajakone@fhnw.ch>
2018-05-16 14:37:49 +0200

    traits init

---
+++ 08_Traits/src/main/java/ch/mse/app/inheritance2/C.java
@@ -0,0 +1,22 @@
+package ch.mse.app.inheritance2;
+
+public class C extends B implements IA, IB {
+	final _A _super;
+
+	C() {
+		_super = new _A(this);
+	}
+
+	@Override
+	public void foo() {
+		System.out.println("C::foo");
+		_super._foo();
+	}
+
+	@Override
+	public void bar() {
+		System.out.println("C::bar");
+		test();
+	}
+
+}
