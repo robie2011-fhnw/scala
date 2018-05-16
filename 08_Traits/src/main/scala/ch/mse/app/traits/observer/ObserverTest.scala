5931de7b5aa8d1578a3f1cd7eae578da4f30b287
Robert Rajakone <robert.rajakone@fhnw.ch>
2018-05-16 15:21:45 +0200

    cleaning

--- 08_Traits/src/main/scala/ch/mse/app/traits/observer/ObserverTest.scala
+++ 08_Traits/src/main/scala/ch/mse/app/traits/observer/ObserverTest.scala
@@ -2,16 +2,30 @@ package ch.mse.app.traits.observer
 
 object ObserverTest {
 
+  trait Observer {
+    def update(subject: Any)
+  }
+
+  trait Observable {
+    var observers : List[Observer] = Nil
+    def addObserver(o: Observer): Unit = observers = o :: observers
+    def notify(a: Any) = observers.foreach(_.update(a))
+    def removeObserver(o: Observer) = observers = observers.filterNot(_ == o)
+  }
+
   class Model(private var value: Int) {
     def getValue = value
     def setValue(value: Int) = { this.value = value }
   }
 
-  class ObservableModel(init: Int) extends Model(init) { // with Observable {
-
+  class ObservableModel(init: Int) extends Model(init) with Observable {
+    override def setValue(value: Int) = {
+      super.setValue(value);
+      notify(value);
+    }
   }
 
-  class Counter {
+  class Counter extends Observer{
     var counter = 0
     def update(subject: Any) = counter += 1
   }
@@ -19,7 +33,7 @@ object ObserverTest {
   def main(args: Array[String]): Unit = {
     val m = new ObservableModel(0)
     val c = new Counter
-    // m.addObserver(c)
+    m.addObserver(c)
     m.setValue(3);
     m.setValue(5);
     println(c.counter) // Ausgabe: 2, da zwei Notifikationen eingegangen sind
